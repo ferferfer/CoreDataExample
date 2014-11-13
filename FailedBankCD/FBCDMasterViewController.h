99e0e84edf3752a3237f08f957ebf17e85bfb172
ferferfer <eskaramujo@gmail.com>
2014-11-13 11:28:00 +0100

    Start of the project

---
+++ FailedBankCD/FBCDMasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  FBCDMasterViewController.h
+//  FailedBankCD
+//
+//  Created by Fernando Garcia Corrochano on 13/11/14.
+//  Copyright (c) 2014 FernandoGarciaCorrochano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCDMasterViewController : UITableViewController
+
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
+
+@end
