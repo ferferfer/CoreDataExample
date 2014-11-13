8f5ebfae2a30d9f62d75490375b160b760ef0115
ferferfer <eskaramujo@gmail.com>
2014-11-13 12:12:50 +0100

    Test a fech in appDelegate and add the model objects

---
+++ FailedBankCD/FailedBankDetails.m
@@ -0,0 +1,19 @@
+//
+//  FailedBankDetails.m
+//  FailedBankCD
+//
+//  Created by Fernando Garcia Corrochano on 13/11/14.
+//  Copyright (c) 2014 FernandoGarciaCorrochano. All rights reserved.
+//
+
+#import "FailedBankDetails.h"
+
+
+@implementation FailedBankDetails
+
+@dynamic zip;
+@dynamic closeDate;
+@dynamic updateDate;
+@dynamic info;
+
+@end
